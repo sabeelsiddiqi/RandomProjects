777bd02ee5beee463499fdc704596842690a07eb
Sabeel Siddiqi <sabeelsiddiqi@Sabeels-MacBook-Pro.local>
2019-09-10 01:16:52 -0400

    extra Changes

--- CalendlyExample.js
+++ CalendlyExample.js
@@ -1,17 +1,28 @@
+//IMPORTS JSFORCE, A LIBRARY TO CONNECT TO SFDC
 var jsforce = require('jsforce');
 var conn = new jsforce.Connection();
 
+//MAIN FUNCTION TO TAKE FORM DATA AND QUALIFY LEAD
 function qualify(company_size, company_name){
-  if (company_size < 100 ){
+  if (company_size < 100 && role != role[0] && role != role[1] && role != role[2] && role != role[3]){
+      
+      //IF COMPANY SIZE IS LESS THAN 100 AND DOESN'T INCLUDE FIRST 4 ROLE TYPE
       this.URL_Redirect()
   } else {
+    
+    //CALLS FUNCTION TO DO SFDC SEARCH ON COMPANY NAME
      var companySearch = this.SFDC_SEARCH(company_name)
      
-     if(companySearch.companyName != null){
+    if(companySearch.companyName != null){
+            // IF THE COMPANY DOESN'T EXIST SHOW THE ROUND ROBIN TEAM CALENDAR
             this.CalendlyRoundRobin();
-     } else if(companySearch.companyName){
+        } else if(companySearch.companyName && !companySearch.accountOwner){
+            // IF THE COMPANY DOES EXIST BUT DOES NOT HAVE AN ACCOUNT OWNER THEN SHOW THE ROUND ROBIN TEAM CALENDAR
+            this.CalendlyRoundRobin();
+        } else if(companySearch.companyName && companySearch.accountOwner){
+            // IF THE COMPANY DOES EXIST AND HAS AN ACCOUNT OWNER THEN SHOW THE AE'S CALENDAR
             this.AECalendar(companySearch.accountOwner);          
-     }
+    }
   }
 }
 
